5336f11382598847a82d01e3651a8ebb45cff390
Gunjan <153367883+GunjanPasricha@users.noreply.github.com>
2023-12-22 14:19:17 +0530

    taskmanager.py

--- taskmanager.py
+++ taskmanager.py
@@ -1,39 +1,88 @@
-tasks=['dfg','asd','iop']
+task_list = ['ghj','jkl','uyt']
 def main_menu():
-    print("""1- add task
-          2- remove task
-          3- upgrade task
-          4- view task
-          5- complete task
-          6- exit
-          """)
-    c = input ("what you want to do:")
-    if c =='1':
-        global x
-        x = input()
-        add_task(x)
-    # if c == '2':
-    #     remove_task()
+        print('''options to select (1-6)
+            1) add Task
+            2) remove Task
+            3) update Task
+            4) view Task
+            5) complete Task
+            6) exit''')
+        choice=(input('enter number from 1-6: '))
+        if choice=="1":
+            add_task()
+        if choice=="2":
+            remove_task()
+        if choice=="3":
+            update_task()
+        if choice=="4":
+            view_task()
+        if choice=="5":
+            complete_task()
+        if choice=="6":
+            out()
+        else:
+            print('please enter valid option:')
+            main_menu()
 
-def conti():
-    b = input('do you want to continue or exit (y or n):')
-    if b=='y' or b=='Y':
-        main_menu()
-    elif b=='n' or b=='N':
-        print('out of program')
 
-def add_task(x):
-    tasks.append(x)
-    if len(tasks)>0:
-        
-        for i in range(1,len(tasks)+1):
-            print(i,end='')
-            print(tasks[i-1])         
+def add_task():
     
-    # x = input('task to remove:')
-    # tasks.remove(x)
-    print(tasks)
+    task = input("enter task: ")
+    task_list.append(task)
+    print(task_list)
     conti()
 
-main_menu()
+ 
+def remove_task():
+    for i in task_list:
+        print(i)
+    try:  
+        task = input("enter task to remove: ")
+        
+        task_list.remove(task)
+        
+            
+        for i in task_list:
+            print(i)
+        conti()
+    except:    
+            print("task is not present in list:")
+            remove_task()
 
+def update_task():
+    for i in task_list:
+        print(i)
+    task = input("enter task to update: ")
+    new_task=input('new task: ')
+    x=task_list.index(task)
+    task_list[x]=new_task
+    for i in task_list:
+        print(i)
+    conti()
+
+def view_task():
+    print("list of pending task")
+    for i in task_list:
+        print(i)
+    conti()
+
+def complete_task():
+    for i in task_list:
+        print(i)
+    task=input('enter done task: ')
+    new_task=f"\033[9m {task} \033[0m"
+    x=task_list.index(task)
+    task_list[x]=new_task
+    for i in task_list:
+        print(i)
+    conti()
+
+def out():
+    print("now you are out of the app ")
+def conti():
+    con=input('want to continue or exit (Y/N): ')
+    if con=='y'or'Y':
+        main_menu()
+    if con=='n'or'N':
+        out()
+main_menu()
